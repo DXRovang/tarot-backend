80b36cd14a67545cadf47428c9e62d5ac49374cd
Alexander Rovang <wolf_vii@yahoo.com>
2021-05-28 12:34:58 -0400

    adds actions to draws_controller

--- app/controllers/api/v1/draws_controller.rb
+++ app/controllers/api/v1/draws_controller.rb
@@ -1,19 +1,35 @@
 class Api::V1::DrawsController < ApplicationController
 
   def index
+    render({json: Draw.all})
+  end
 
+  def create
+    draw = Draw.create(draw_params)
+    render json: draw
+    # draw = Draw.new(draw_params)
+    # if draw.save
+    #   render json: draw
+    # else 
+    #   render json: {error: "Error creating draw"}
+    # end
   end
 
   def show
-    
+    draw = Draw.find_by(id: params[:id])
+    render json: draw
   end
 
   def update
-    
+    # draw = Draw.find_by(id: params[:id])
+    # draw.update(draw_params)
+    # render json: draw
   end
 
   def destroy
-    
+    draw = Draw.find_by(id: params[:id])
+    draw.destroy
+    render json: Draw.all
   end
 
   private
