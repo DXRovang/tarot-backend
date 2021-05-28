6bc4ddae2ce4a4b0969e3fe99c6f71738c4bfa65
Alexander Rovang <wolf_vii@yahoo.com>
2021-05-28 11:41:43 -0400

    seeds database

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::CardsController < ApplicationController
+
+  def index
+    # cards = RestClient.get 'https://raw.githubusercontent.com/ronniekram/tarot-js-v2/main/front/src/cards.json'
+    # card_array = JSON.parse(cards)["cards"]
+    # binding.pry
+    render({json: Card.all})
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,17 @@
-require ‘rest-client’
+require 'rest-client'
 
 cards = RestClient.get 'https://raw.githubusercontent.com/ronniekram/tarot-js-v2/main/front/src/cards.json'
 
-card_array = JSON.parse(cards)["results"]
+card_array = JSON.parse(cards)["cards"]
 
 card_array.each do |card|
-  binding.pry
   Card.create(
-    name: card["name"]
+    cardType: card["cardType"],
+    name: card["name"],
+    summary: card["summary"],
+    image: card["image"],
+    meaning_up: card["meaning_up"],
+    meaning_rev: card["meaning_rev"],
+    desc: card["desc"]
   )
 end
