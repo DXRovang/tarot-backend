8370f54d7398c9d4210ce447a3ceaaf4f2611475
Alexander Rovang <wolf_vii@yahoo.com>
2021-05-28 12:23:43 -0400

    adds index/show and params for cards

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -6,4 +6,24 @@ class Api::V1::CardsController < ApplicationController
     # binding.pry
     render({json: Card.all})
   end
+
+  def show
+    card = Card.find_by(id: params[:id])
+    render json: card
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(
+      :name,
+      :created_at,
+      :draw_id,
+      :cardType,
+      :summary,
+      :image,
+      :meaning_up,
+      :meaning_rev,
+      :desc
+    )
+  end
 end
--- app/controllers/api/v1/draws_controller.rb
+++ app/controllers/api/v1/draws_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::DrawsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def draw_params
+    params.require(:draw).permit(:layout, :reading)
+  end
+
 end
